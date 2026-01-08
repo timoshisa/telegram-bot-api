3ee63aaf744832b5a7750ea2f9a4d47b38e95f4b
Tim Behrsin <tim.behrsin@grexie.com>
2026-01-08 22:50:20 +0000

    message_thread_id

--- configs.go
+++ configs.go
@@ -265,6 +265,7 @@ func (CloseConfig) params() (Params, error) {
 // BaseChat is base type for all chat config types.
 type BaseChat struct {
 	ChatID                   int64 // required
+	MessageThreadID          int64
 	ChannelUsername          string
 	ReplyToMessageID         int
 	ReplyMarkup              interface{}
@@ -276,6 +277,7 @@ func (chat *BaseChat) params() (Params, error) {
 	params := make(Params)
 
 	params.AddFirstValid("chat_id", chat.ChatID, chat.ChannelUsername)
+	params.AddNonZero64("message_thread_id", chat.MessageThreadID)
 	params.AddNonZero("reply_to_message_id", chat.ReplyToMessageID)
 	params.AddBool("disable_notification", chat.DisableNotification)
 	params.AddBool("allow_sending_without_reply", chat.AllowSendingWithoutReply)
